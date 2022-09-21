003e24b4785704ca40764c97e458625327762fe3
vinferno <vinsonfernandez@gmail.com>
2022-09-20 21:09:36 -0700

    day01

--- day01/js/main.js
+++ day01/js/main.js
@@ -1,10 +1,10 @@
+// alert when button clicked
 document.querySelector("button").addEventListener("click", () => {
   alert();
 });
 
-
 // create elements
-const h1 = document.createElement('h1')
+const h1 = document.createElement('h1');
 
 // place on page
 document.body.append(h1);
@@ -16,9 +16,9 @@ h1.innerHTML = 'Hello World';
 h1.style.color = 'red';
 
 // add a class
-h1.classList.add('big')
+h1.classList.add('big');
 
 // add listeners
 h1.addEventListener('click', () => {
-  h1.innerHTML = 'world'
-})
+  h1.innerHTML = 'world';
+});
