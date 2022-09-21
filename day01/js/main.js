a4f6ab2e8e28dbcde47e11588412e23b9cc569ee
vinferno <vinsonfernandez@gmail.com>
2022-09-20 21:00:51 -0700

    day01

--- day01/js/main.js
+++ day01/js/main.js
@@ -1,3 +1,24 @@
 document.querySelector("button").addEventListener("click", () => {
   alert();
 });
+
+
+// create elements
+const h1 = document.createElement('h1')
+
+// place on page
+document.body.append(h1);
+
+// update text
+h1.innerHTML = 'Hello World';
+
+// interact with css
+h1.style.color = 'red';
+
+// add a class
+h1.classList.add('big')
+
+// add listeners
+h1.addEventListener('click', () => {
+  h1.innerHTML = 'world'
+})
